a8459de40aae65e36f5300c5c0b90f4caaf1c4e7
David Alves Dutra <dadutra@hotmail.com>
2024-09-29 19:14:39 -0300

    Jwt Token Service added.

--- stateless/stateless-auth-api/src/main/java/com/microservice/stateless_auth_api/core/service/JwtService.java
+++ stateless/stateless-auth-api/src/main/java/com/microservice/stateless_auth_api/core/service/JwtService.java
@@ -1,9 +1,82 @@
 package com.microservice.stateless_auth_api.core.service;
 
+import com.microservice.stateless_auth_api.core.model.User;
+import com.microservice.stateless_auth_api.infra.exception.AuthenticationException;
+import com.microservice.stateless_auth_api.infra.exception.ValidationException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
+
+    private static final Integer ONE_DAY_IN_HOURS = 24;
+    private static final String EMPTY_SPACE = " ";
+    private static final Integer TOKEN_INDEX = 1;
+
+    @Value("${app.token.secretKey}")
+    private String secretKey;
+
+
+    public String createToken(User user) {
+        var data = new HashMap<String, String>();
+
+        data.put("id", user.getId().toString());
+        data.put("username", user.getUserName());
+
+        return Jwts.builder()
+                .setClaims(data)
+                .setExpiration(generateExpiresAt())
+                .signWith(generateSign())
+                .compact();
+    }
+
+    public void validateAccessToken(String token) {
+        var accessToken = extractToken(token);
+        try {
+            Jwts
+                    .parser()
+                    .verifyWith(generateSign())
+                    .build()
+                    .parseSignedClaims(accessToken)
+                    .getPayload();
+        } catch (Exception ex) {
+            throw new AuthenticationException("Invalid token: " + ex.getMessage());
+        }
+    }
+
+    private String extractToken(String token) {
+        if (isEmpty(token)) {
+            throw new ValidationException("The access token was not informed.");
+        }
+
+        if (token.contains(EMPTY_SPACE)) {
+            return token.split(EMPTY_SPACE)[TOKEN_INDEX];
+        }
+
+        return token;
+    }
+
+    private Date generateExpiresAt() {
+        return Date.from(
+                LocalDateTime.now()
+                        .plusHours(ONE_DAY_IN_HOURS)
+                        .atZone(ZoneId.systemDefault()).toInstant()
+        );
+    }
+
+    private SecretKey generateSign() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
 }
