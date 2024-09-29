97712a1889e38f1d7c7adba90fac4d54cfdf524a
David Alves Dutra <dadutra@hotmail.com>
2024-09-29 20:09:47 -0300

    Controller auth added.

---
+++ stateless/stateless-auth-api/src/main/java/com/microservice/stateless_auth_api/core/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.microservice.stateless_auth_api.core.controller;
+
+import com.microservice.stateless_auth_api.core.DTO.AuthRequest;
+import com.microservice.stateless_auth_api.core.DTO.TokenDTO;
+import com.microservice.stateless_auth_api.core.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("login")
+    public TokenDTO login(@RequestBody AuthRequest request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("validate")
+    public TokenDTO validateToken(@RequestHeader String accessToken) {
+        return authService.validateToken(accessToken);
+    }
+}
